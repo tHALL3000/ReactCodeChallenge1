0da68159bc31192ab9bd4df2157948aca2a9e175
tHALL3000 <80265634+tHALL3000@users.noreply.github.com>
2021-06-26 20:14:17 -0600

    first step creating containers for data to be passed in

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ScotchInfoBar from './ScotchInfoBar';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
 
 /**
  * Our data
  * ------------------------
  */
 const user = {
-  name: 'Chris on Code',
-  location: 'Las Vegas',
-  foodType: 'Everything',
+  name: "Chris on Code",
+  location: "Las Vegas",
+  foodType: "Everything",
   age: 28,
-  likes: 'Coding into the wee hours of the morning',
-  twitterUsername: 'chrisoncode',
+  likes: "Coding into the wee hours of the morning",
+  twitterUsername: "chrisoncode",
   avatar:
-    'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'
+    "https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png"
 };
 
 /**
@@ -26,12 +26,36 @@ function App() {
   return (
     <div className="App">
       {/* Show user data here */}
-      <div className="user-deets">display user info here. happy coding!</div>
+      <div className="user-deets">
+        '
+        <img src={user.avatar} alt={user.name} />
+        <h3> {user.name} </h3>
+        <p>
+          <strong> Location </strong>
+          {user.location}
+        </p>
+        <p>
+          <strong> Eats</strong>
+          {user.foodType}
+        </p>
+        <p>
+          <strong> Age</strong>
+          {user.age}
+        </p>
+        <p>
+          <strong> Likes</strong>
+          {user.likes}
+        </p>
+        <p>
+          <strong> Twitter</strong>
+          <a>@{user.twitterUsername}</a>
+        </p>
+      </div>
 
       <ScotchInfoBar />
     </div>
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
